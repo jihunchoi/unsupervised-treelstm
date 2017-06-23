900c1e53ba373d54d921c6e67161309d03c93094
jhchoi <jhchoi@europa.snu.ac.kr>
2017-06-24 01:55:23 +0900

    Fix an encoding bug on loading GloVe vectors

--- utils/glove.py
+++ utils/glove.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def load_glove(path, vocab, init_weight: np.array):
     glove_weight = init_weight.copy()
-    with open(path, 'r') as f:
+    with open(path, 'r', encoding='latin1') as f:
         for line in f:
             word, *values = line.split()
             if vocab.has_word(word):
